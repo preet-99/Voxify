5b9bcbe301350506e935c617c0ae6f176b5b3d05
Preet Vishwakarma <159623026+preet-99@users.noreply.github.com>
2025-10-01 23:09:56 +0530

    Fix some bugs

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,7 +3,7 @@ export default function CTA() {
     <section className="py-16 bg-[#c4e5ffb5] text-center rounded-xl mx-6 mt-12">
       <h2 className="text-3xl font-bold mb-4">Ready to Try AI Tools?</h2>
       <p className="mb-6">Convert video, image, text, or speech instantly with our smart AI.</p>
-      <a href="#tools" className="bg-white text-primary px-6 py-3 rounded-full font-semibold hover:bg-green-300 transition">
+      <a href="#features" className="bg-white text-primary px-6 py-3 rounded-full font-semibold hover:bg-green-300 transition">
         Start Now
       </a>
     </section>
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,36 +1,71 @@
 import { FaUser, FaEnvelope } from "react-icons/fa";
-
+import { ToastContainer, toast } from "react-toastify";
+import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css"; // Import the CSS
 
 export default function FeedbackForm() {
+  const [form, setForm] = useState({name: "", email:"", feedback:""})
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+
+    toast.success("Thank you for your feedback!", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+
+    setForm({name: "", email:"", feedback:""})
+  };
+
+  const handleChange = (e)=>{
+    setForm({[e.target.name]: e.target.value})
+  }
+
   return (
     <section className="py-16 px-6 bg-background-dark rounded-xl mx-6 my-12 shadow-lg">
-      <h2 className="text-3xl font-bold text-center mb-8 text-primary">Give Us Your Feedback</h2>
-      
-      <form className="max-w-3xl mx-auto flex flex-col gap-6">
-        {/* Name Input */}
+     
+      <ToastContainer />
+      <h2 className="text-3xl font-bold text-center mb-8 text-primary">
+        Give Us Your Feedback
+      </h2>
+
+      <form className="max-w-3xl mx-auto flex flex-col gap-6" onSubmit={handleSubmit}>
         <input
           type="text"
+          value={form.name}
+          onChange={handleChange}
+          name="name"
           placeholder="Your Name"
-          className="p-4 rounded-lg border border-black bg-background  text-black placeholder-gray-400 "
+          className="p-4 rounded-lg border border-black bg-background text-black placeholder-gray-400 "
         />
 
-        {/* Email Input */}
         <input
           type="email"
+          value={form.email}
+          name="email"
+          onChange={handleChange}
           placeholder="Your Email"
           className="p-4 rounded-lg border border-black bg-background text-black placeholder-gray-400 "
         />
 
-        {/* Feedback Textarea */}
         <textarea
           rows="5"
+          value={form.feedback}
+          onChange={handleChange}
+          name="feedback"
           placeholder="Write your feedback here..."
           className="p-4 rounded-lg border border-black bg-background text-black placeholder-gray-400 "
         ></textarea>
 
         {/* Submit Button */}
         <button
-          type="submit"
+          type="submit" 
+          disabled={!form.name && !form.email && !form.feedback}
           className="bg-blue-700 text-white px-6 py-3 rounded-lg font-semibold hover:opacity-90 transition cursor-pointer"
         >
           Submit Feedback
